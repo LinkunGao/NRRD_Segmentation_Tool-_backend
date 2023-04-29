2181f071a0faf8cdad7958c42ab62d82322357d4
skycoco <skycoco1994@gmail.com>
2023-04-29 16:38:05 +1200

    create new class to store calculating volume.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .setup import Config
+from .setup import Config, TumourData
 from .convert import convert_to_nii, convert_to_nii_full_channels, convert_to_nii_sigel_channel, convert_to_nrrd_sigel_channel
